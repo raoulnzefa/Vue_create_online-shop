673d3103183f76fc0528a03e9d47c2b7a2ef632a
Maks <59100893+Xokage123@users.noreply.github.com>
2020-11-02 00:10:42 +0300

    initial repo

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import App from './App.vue';
+import data from './homeWork/data';
+import alertMessage from './homeWork/alert';
+
+Vue.config.productionTip = false;
+
+new Vue({
+    render: (h) => h(App),
+}).$mount('#app');
+// alertMessage(data.message_1);
+// alertMessage(data.message_2);
